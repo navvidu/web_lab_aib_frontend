278dba57f5e42e913ecc0566371bf86f76145f83
Daria Amelyakina <118108694+navvidu@users.noreply.github.com>
2023-11-01 19:58:33 +0300

    Delete labs/Lab_07_refactoring/solution/script.js

--- labs/Lab_07_refactoring/solution/script.js
+++
@@ -1,9 +0,0 @@
-//№1
-var firstVariable = prompt('var one');
-var secondVariable = prompt('var two');
-if (firstVariable === secondVariable)
-    console.log('equally');
-else console.log('not equally');
-var stringVariable = 'world';
-var generalVariable = firstVariable + stringVariable;
-console.log(generalVariable);
