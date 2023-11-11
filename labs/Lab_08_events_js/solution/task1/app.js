1a1b83b41cc697746e6b8675d224f18b83e50ec8
Daria Amelyakina <118108694+navvidu@users.noreply.github.com>
2023-11-11 13:45:41 +0300

    Add files via upload

---
+++ labs/Lab_08_events_js/solution/task1/app.js
@@ -0,0 +1,19 @@
+
+let redButton = document.getElementById("buttonRed");
+redButton.addEventListener("click",()=>{
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "red";
+});
+
+let greenButton = document.getElementById("buttonGreen");
+greenButton.addEventListener("click",()=>{
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "green";
+});
+
+let blueButton = document.getElementById("buttonBlue");
+blueButton.addEventListener("click",()=>{
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "blue";
+});
+
