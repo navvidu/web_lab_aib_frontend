cd4ec1f7ad1ff66c3079a1d80bea7126f67031bd
Daria Amelyakina <118108694+navvidu@users.noreply.github.com>
2023-11-12 18:10:38 +0300

    Update app.js

--- labs/Lab_08_events_js/solution/task2/app.js
+++ labs/Lab_08_events_js/solution/task2/app.js
@@ -2,13 +2,13 @@
 let inputRed = document.getElementById('red');
 let inputGreen = document.getElementById('green');
 let inputBlue = document.getElementById('blue');
-let squareColor = document.getElementById('square');
+let colorSquare = document.getElementById('square');
 
 function colorUpdate() {
     let redValue = inputRed.value;
     let greenValue = inputGreen.value;
     let blueValue = inputBlue.value;
-    squareColor.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    colorSquare.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 }
 
 inputRed.addEventListener('input', colorUpdate);
